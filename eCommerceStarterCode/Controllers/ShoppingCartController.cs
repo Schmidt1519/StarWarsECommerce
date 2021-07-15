2699fc65760bd179536000bedb9b8be110cb1e28
Chris Willett <cmwillett87@gmail.com>
2021-07-15 14:46:18 -0400

    Added the get all and post method for categories

--- eCommerceStarterCode/Controllers/ShoppingCartController.cs
+++ eCommerceStarterCode/Controllers/ShoppingCartController.cs
@@ -1,15 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using eCommerceStarterCode.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace eCommerceStarterCode.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/cart")]
     [ApiController]
     public class ShoppingCartController : ControllerBase
     {
+        private ApplicationDbContext _context;
+        public ShoppingCartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET Shopping Cart
+        [HttpGet("{id:int}")]
+        public IActionResult GetShoppingCart(int id)
+        {
+            var shoppingCart = _context.ShoppingCarts.Find(id);
+            return Ok(shoppingCart);
+        }
     }
 }
